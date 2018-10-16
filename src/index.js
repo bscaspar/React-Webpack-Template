778e10d5ff6b2930aabb8b1bb567a6e0df677987
Brady Caspar <Brady@Bradys-MacBook-Air.local>
2018-10-16 14:38:36 -0700

    add react-dom, hello world

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-const helloWorld = "Hello World";
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { }
+    }
+    
+    render(){
+        return(
+            <div>
+                Hello world!
+            </div>
+        )
+    }
+
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
